9250be364765c9ed34b27e6964076820c52124bf
chakit-pragod <chakit.p@kkumail.com>
2024-10-06 21:01:12 +0700

    merge AomsinNews

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var transactionRouter = require('./routes/transaction');
+const announceRoutes = require('./routes/announce'); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -161,6 +162,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/transaction', transactionRouter)
+app.use(announceRoutes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
